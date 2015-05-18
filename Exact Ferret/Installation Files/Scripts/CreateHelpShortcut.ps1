d0a1af5c5f04d422f80ee03a4f8838ebda68d787
Daniel Pace <danielpace6@gmail.com>
2015-05-17 18:46:39 -0700

    Update help shortcut to the new help URL.

--- Exact Ferret/Installation Files/Scripts/CreateHelpShortcut.ps1
+++ Exact Ferret/Installation Files/Scripts/CreateHelpShortcut.ps1
@@ -1,5 +1,5 @@
 $shortcut_name = "Exact Ferret Help"
-$shortcut_target = "http://exactferret.kangaroostandard.com/help/"
+$shortcut_target = "https://www.kangaroostandard.com/ExactFerret/help/"
 $sh = new-object -com "WScript.Shell"
 $p = $env:ALLUSERSPROFILE + "\Microsoft\Windows\Start Menu\Programs\Exact Ferret"
 $lnk = $sh.CreateShortcut( (join-path $p $shortcut_name) + ".lnk" )  
